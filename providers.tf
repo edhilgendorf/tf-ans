344d120b3f3d06919c100b0b3f40e0f059e2c3aa
edward <edward@hilgendorf.me>
2021-08-15 18:05:29 -0700

    remove shared file

--- providers.tf
+++ providers.tf
@@ -1,13 +1,11 @@
 provider "aws" {
   profile = var.profile
   region  = var.region-master
-  shared_credentials_file = "~/.aws/credentials"
   alias   = "region-master"
 }
 
 provider "aws" {
   profile = var.profile
   region  = var.region-worker
-  shared_credentials_file = "~/.aws/credentials"
   alias   = "region-worker"
 }
