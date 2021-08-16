8d3e240dbf64baeed1595758b403ca30dfa16073
edward <edward@hilgendorf.me>
2021-08-15 18:30:02 -0700

    fix region

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+  region        = var.region-master
+
 
   tags = {
     Name = "ExampleAppServerInstance"
