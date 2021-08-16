d2d0292864f541e373f9d6fd7fce78ccfb6ead31
edward <edward@hilgendorf.me>
2021-08-15 18:33:10 -0700

    update

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
-  region        = var.region-master
-
 
   tags = {
     Name = "ExampleAppServerInstance"
--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  profile = var.profile
+  #profile = var.profile
   region  = var.region-master
   alias   = "region-master"
 }
 
 provider "aws" {
-  profile = var.profile
+  #profile = var.profile
   region  = var.region-worker
   alias   = "region-worker"
 }
