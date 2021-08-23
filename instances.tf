9e0beb25c572da3b44f232d5e968c2369a00c5dc
edward <edward@hilgendorf.me>
2021-08-23 11:26:16 -0700

    update vars, remove dupe ssh key from instances

--- instances.tf
+++ instances.tf
@@ -20,7 +20,8 @@ resource "aws_key_pair" "worker-key" {
   provider = aws.region-worker
   key_name = "jenkins"
   #public_key = file("~/.ssh/id_rsa.pub")
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDRLTIuUEFLRj7+PbhWbuWEigBKBk7ej2XNQCEyyvsLCU5ja1OlnnMnzhiVptlIK29WQ2+UiWC/pd+reN7tcsu3XWf4iVOjfxavBPFfZw8P0Ywk0lfaQAvPIjfbBfdF2NDNOTjWL8vL/S8xz6pE9FVwmHnXrQePPTn5i54yotk7Am/Vf+edNzG/dnHuUTJRkiKKsy+akR+Zx7LuXi3m1bbCpuCGmlMowIX+Cjn4nQApycQla2qizKgj1/u6ViPc0cVBaVkSvWOdiQnTmThthIDEiCuBAcK+0rGtMnvwAfoJ8z+p2Lp0ajrPZU9YAKncTpKb/W245qNR9EAaHDderMB2++5lZHvbssEZOeWqeUstuAOvZv2Wdi+47bYZL5iYfUAiBWKricGAHGBHmtAausuwCiDfCjdkOu1CJI8oORH3KLjhWrR30y5eXs/mGQIqWlkBmGJvcHB+vvzlASD0IVfUbHI6Qs6Zdf/3foej8TLyBFEzQgYnBaQNoLNBgJ3uwjU= edward@fireside"
+  public_key = file("ssh/id_rsa.pub")
+  #public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDRLTIuUEFLRj7+PbhWbuWEigBKBk7ej2XNQCEyyvsLCU5ja1OlnnMnzhiVptlIK29WQ2+UiWC/pd+reN7tcsu3XWf4iVOjfxavBPFfZw8P0Ywk0lfaQAvPIjfbBfdF2NDNOTjWL8vL/S8xz6pE9FVwmHnXrQePPTn5i54yotk7Am/Vf+edNzG/dnHuUTJRkiKKsy+akR+Zx7LuXi3m1bbCpuCGmlMowIX+Cjn4nQApycQla2qizKgj1/u6ViPc0cVBaVkSvWOdiQnTmThthIDEiCuBAcK+0rGtMnvwAfoJ8z+p2Lp0ajrPZU9YAKncTpKb/W245qNR9EAaHDderMB2++5lZHvbssEZOeWqeUstuAOvZv2Wdi+47bYZL5iYfUAiBWKricGAHGBHmtAausuwCiDfCjdkOu1CJI8oORH3KLjhWrR30y5eXs/mGQIqWlkBmGJvcHB+vvzlASD0IVfUbHI6Qs6Zdf/3foej8TLyBFEzQgYnBaQNoLNBgJ3uwjU= edward@fireside"
 }
 #Create and bootstrap EC2 in us-east-1
 resource "aws_instance" "jenkins-master" {
