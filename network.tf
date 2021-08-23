2b6f3ec2f18d557ed37b399df80318665bb76827
edward <edward@hilgendorf.me>
2021-08-22 17:36:13 -0700

    clean up and label network.tf, update readme with network parts

--- network.tf
+++ network.tf
@@ -9,7 +9,6 @@ resource "aws_vpc" "vpc_master" {
     Name = "master-vpc-jenkins"
   }
 }
-
 #Create VPC in us-west-2
 resource "aws_vpc" "vpc_master_oregon" {
   provider             = aws.region-worker
@@ -28,7 +27,6 @@ resource "aws_internet_gateway" "igw" {
   provider = aws.region-master
   vpc_id   = aws_vpc.vpc_master.id
 }
-
 #Create IGW in us-west-2
 resource "aws_internet_gateway" "igw-oregon" {
   provider = aws.region-worker
@@ -50,7 +48,6 @@ resource "aws_subnet" "subnet_1" {
   vpc_id            = aws_vpc.vpc_master.id
   cidr_block        = "10.0.1.0/24"
 }
-
 #create subnet #2 in us-east-1
 resource "aws_subnet" "subnet_2" {
   provider          = aws.region-master
@@ -58,7 +55,6 @@ resource "aws_subnet" "subnet_2" {
   vpc_id            = aws_vpc.vpc_master.id
   cidr_block        = "10.0.2.0/24"
 }
-
 #create subnet #1 in us-west-2
 resource "aws_subnet" "subnet_1_oregon" {
   provider   = aws.region-worker
@@ -66,8 +62,6 @@ resource "aws_subnet" "subnet_1_oregon" {
   cidr_block = "192.168.1.0/24"
 }
 ########### SUBNET END ####################
-
-
 
 ########### PEERING START ####################
 #Initiate Peering connection request from us-east-1
@@ -78,14 +72,15 @@ resource "aws_vpc_peering_connection" "useast1-uswest2" {
   peer_region = var.region-worker
 
 }
-
 #Accept VPC peering request in us-west-2 from us-east-1
 resource "aws_vpc_peering_connection_accepter" "accept_peering" {
   provider                  = aws.region-worker
   vpc_peering_connection_id = aws_vpc_peering_connection.useast1-uswest2.id
   auto_accept               = true
 }
+########### PEERING END ####################
 
+########### ROUTING TABLE START ####################
 #Create route table in us-east-1
 resource "aws_route_table" "internet_route" {
   provider = aws.region-master
@@ -105,14 +100,12 @@ resource "aws_route_table" "internet_route" {
     Name = "Master-Region-RT"
   }
 }
-
 #Overwrite default route table of VPC(Master) with our route table entries
 resource "aws_main_route_table_association" "set-master-default-rt-assoc" {
   provider       = aws.region-master
   vpc_id         = aws_vpc.vpc_master.id
   route_table_id = aws_route_table.internet_route.id
 }
-
 #Create route table in us-west-2
 resource "aws_route_table" "internet_route_oregon" {
   provider = aws.region-worker
@@ -132,10 +125,10 @@ resource "aws_route_table" "internet_route_oregon" {
     Name = "Worker-Region-RT"
   }
 }
-
 #Overwrite default route table of VPC(Worker) with our route table entries
 resource "aws_main_route_table_association" "set-worker-default-rt-assoc" {
   provider       = aws.region-worker
   vpc_id         = aws_vpc.vpc_master_oregon.id
   route_table_id = aws_route_table.internet_route_oregon.id
 }
+########### ROUTING TABLE END ####################
