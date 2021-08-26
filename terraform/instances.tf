488cc52cab5a478b100e4813a90e7d1f02e91a11
edward <edward@hilgendorf.me>
2021-08-25 21:23:57 -0700

    test ans

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,5 +1,4 @@
 #Get Linux AMI ID using SSM Parameter endpoint in us-east-1
-##deploy
 data "aws_ssm_parameter" "linuxAmi" {
   provider = aws.region-master
   name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
