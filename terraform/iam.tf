3d601c62a8933be4955ea1e4831e4349446243dc
edward <edward@hilgendorf.me>
2021-09-01 13:47:44 -0700

    add iam role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,25 +1,25 @@
-#resource "aws_iam_role" "test_role" {
-#  name = "test_role"
-#  assume_role_policy = <<EOF
-#{
-#  "Version": "2012-10-17",
-#  "Statement": [
-#    {
-#      "Action": "sts:AssumeRole",
-#      "Principal": {
-#        "Service": "ec2.amazonaws.com"
-#      },
-#      "Effect": "Allow",
-#      "Sid": ""
-#    }
-#  ]
-#}
-#EOF
-#  tags = {
-#      tag-key = "tag-value"
-#  }
-#}
-#
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+  tags = {
+      tag-key = "tag-value"
+  }
+}
+
 #resource "aws_iam_instance_profile" "test_profile" {
 #  name = "test_profile"
 #  role = "${aws_iam_role.test_role.name}"
