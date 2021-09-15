7b6905b37decf246f2e2f5a3ae00bc48ebbae2d2
edward <edward@hilgendorf.me>
2021-09-15 10:03:26 -0700

    add provider to bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "misc-data" {
-  bucket = "hilgendorf-misc-data"
-  acl    = "private"
+  provider  = aws.region-master
+  bucket    = "hilgendorf-misc-data"
+  acl       = "private"
 
   tags = {
     Name        = "misc data"
