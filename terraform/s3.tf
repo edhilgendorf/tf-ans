47c35c1bfba489cf115514b6681cfe5f9ac1e241
edward <edward@hilgendorf.me>
2021-09-15 10:13:17 -0700

    updated IAM, re-attempting to add bucket.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "misc-data" {
   bucket    = "hilgendorf-misc-data"
   acl       = "private"
 
-#  tags = {
-#    Name        = "misc data"
-#  }
+  tags = {
+    Name        = "misc data"
+  }
 }
