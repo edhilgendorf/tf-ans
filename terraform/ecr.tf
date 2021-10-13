c88937e4b93c64fae3366e4bb97f16eb40fb97ea
edward <edward@hilgendorf.me>
2021-10-12 18:45:20 -0700

    add region to ECR

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "images" {
   name                 = "docker_images"
   image_tag_mutability = "MUTABLE"
+  region = var.region-master
 
   image_scanning_configuration {
     scan_on_push = true
