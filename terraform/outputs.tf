17576c754e62baf1f2c2affe34b4d5474db8dd8a
edward <edward@hilgendorf.me>
2021-10-12 21:54:59 -0700

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,14 +8,3 @@ output "Devops-Worker-Public-IPs" {
     instance.id => instance.public_ip
   }
 }
-
-output "Devops-Main-Node-Public-IP" {
-  value = aws_instance.devops-master.public_ip
-}
-
-output "Devops-Worker-Public-IPs" {
-  value = {
-    for instance in aws_instance.devops-worker-oregon :
-    instance.id => instance.public_ip
-  }
-}
