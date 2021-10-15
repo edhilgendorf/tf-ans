4e5649e0684bbf8779396f8bc3c4dd1826b4ad22
edward <edward@hilgendorf.me>
2021-10-15 16:59:14 -0700

    add depends on

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 resource "aws_ecr_repository" "docker_images" {
   name                 = "docker_images"
-  image_tag_mutability = "MUTABLE"
+#  image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -27,6 +27,10 @@ resource "aws_ecs_task_definition" "my_first_task" {
   memory                   = 512         # Specifying the memory our container requires
   cpu                      = 256         # Specifying the CPU our container requires
   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+
+  depends_on = [
+   aws_ecr_repository.docker_images,
+  ]
 }
 
 resource "aws_iam_role" "ecsTaskExecutionRole" {
