d5c159ee3c449c3445f85bd3625284a6bf15b0e3
edward <edward@hilgendorf.me>
2021-10-16 15:47:42 -0700

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -53,3 +53,16 @@ resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
   role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_ecs_service" "my_first_service" {
+  name            = "my-first-service"                             # Naming our first service
+  cluster         = "${aws_ecs_cluster.my_cluster.id}"             # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Setting the number of containers we want deployed to 3
+
+  network_configuration {
+    subnets          = aws_subnet.subnet_1
+    assign_public_ip = true # Providing our containers with public IPs
+  }
+}
