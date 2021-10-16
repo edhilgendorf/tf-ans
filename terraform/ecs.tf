5ed7823c5ed888adaba37d28a8dfbe1e5aef0446
edward <edward@hilgendorf.me>
2021-10-16 16:08:21 -0700

    add LB for ECS

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -60,7 +60,12 @@ resource "aws_ecs_service" "my_first_service" {
   task_definition = "${aws_ecs_task_definition.my_first_task.arn}" # Referencing the task our service will spin up
   launch_type     = "FARGATE"
   desired_count   = 3 # Setting the number of containers we want deployed to 3
-
+    
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.my_first_task.family}"
+    container_port   = 3000 # Specifying the container port
+  }
   network_configuration {
     subnets          = ["${aws_subnet.subnet_1.id}"]
     assign_public_ip = true # Providing our containers with public IPs
--- terraform/network.tf
+++ terraform/network.tf
@@ -122,3 +122,25 @@ resource "aws_main_route_table_association" "set-worker-default-rt-assoc" {
   vpc_id         = aws_vpc.vpc_master_oregon.id
   route_table_id = aws_route_table.internet_route_oregon.id
 }
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_vpc.vpc_master.id}" # Referencing the default VPC
+  health_check {
+    matcher = "200,301,302"
+    path = "/"
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = "${aws_alb.application_load_balancer.arn}" # Referencing our load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Referencing our tagrte group
+  }
+}
